9197a4a1a27d73259d5117005f079853f28287a4
Dave Hart <daveihart@hotmail.com>
2021-01-15 15:25:26 +0000

    cleanup

--- create_proxy_rule01.0.sh
+++ create_proxy_rule01.0.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
-
-#Local declarations based on Env variables passed from gocd server
 sudo application=${DEMO_URL_APPLICATION,,}
 envcode=${DEMO_URL_ENVCODE,,}
 internalhostname=${DEMO_URL_INTERNALHOSTNAME,,}
 internalport=$DEMO_URL_INTERNALPORT
-
 type="https"
 echo "application : "$application
 echo "envcode : "$envcode
 echo "internalhostname : "$internalhostname
 echo "internalport : "$internalport
-
-##### Calculated vars####################
 ProxyPass=$type"://"$internalhostname":"$internalport"/"
 ProxyPassReverse=$type"://"$internalhostname":"$internalport"/"
-
-#####   start - static variables    #####
 supp_apps=("first_app" "second_app" "third_app") # Used to validate supported rule is being defined
 ip="0.0.0.0" # External IP used for Route53 to redirect traffic
 type="A"
@@ -25,10 +18,7 @@ dt=`date +%F"-"%H"-"%M`
 hostedzoneid="XXXXXXXXXXXXXXXXX" # Your AWS Hosted Zone id
 dnssuffix=".example.com"    #suffix to add to entry
 comment="Programmatically created DNS Entry for "$record" created on "$dt
-#####   end     ###################################
-
 checkpath="/etc/httpd/conf.d/"
-
 initialrecord=$envcode$application
 
 function validate_app
@@ -72,7 +62,6 @@ function validate_app
 
 function build_record
 {
-    #build the A record based on the provided variables calculating the next available address
     found=""
     rec_count=0
     existing_rec_array=()
@@ -80,32 +69,25 @@ function build_record
     all_records=(`aws route53 list-resource-record-sets --hosted-zone-id $hostedzoneid --output text --query "ResourceRecordSets[*].Name"`)
     echo "Route53 has ${#all_records[@]} registered A Records for $dnssuffix"
     if [ ${#all_records[@]} -gt 0 ]; then
-        #echo "Identifying existing records with similar prefix for processing"
         for record in "${all_records[@]}"
         do
-		#echo $record" is being checked!"
             if [[ "$record" == *"$initialrecord"* ]]; then
-                #echo $record" already exists"
                 existing_rec_array+=($record)
                 found="yes"
                 rec_count=$(($rec_count+1))
             fi
         done
     fi
-	#echo "Found variable set as "$found
     if [ "$found" = "" ]; then
-        #echo "No records found for $initialrecord"
         record=$initialrecord"01"
         checkurl=$record$dnssuffix
 	echo "DNS A Record will be created for "$record
     else
-		# There are records following this name, step through and work out available suffix
         available=""
         available_count=1
         while [[ "$available" != "true" && "$available_count" -le 20 ]];
             do
                 printf -v pad_available_count "%02d" $available_count
-                #build the string to check for
                 checkurl=$initialrecord$pad_available_count$dnssuffix
                 checkurlprefix=$initialrecord$pad_available_count
                 checkurldot=$checkurl"."
@@ -125,7 +107,6 @@ function build_record
 
 function check_conf_d
 {
-    #ensure there are no existing configuration files with this name. Existing suggests an orphones file as the names should match a route53 rule
     checkfile=$checkpath$checkurlprefix".conf"
     echo "Checking for existing conf file "$checkfile
     if [ -f "$checkfile" ]; then
@@ -134,13 +115,11 @@ function check_conf_d
     else
         echo "No existing conf files for "$checkurlprefix" , continuing"
     fi
-
 }
 
 function build_proxy_rule
 {
     echo "creating proxy rule for "$record
-    #create a filename
     conf_file=$checkpath$record".conf"
     if [ "$application" == "first_app" ] ;then
         cat > ${conf_file} << EOF
@@ -206,7 +185,6 @@ EOF
 
 function update_route53
 {
-    # First generate a json file with required details as Route53 cli uses this as the input stream
     TMPFILE=$(mktemp testXXX.json)
     echo "Temporary file created : "$TMPFILE
     cat > ${TMPFILE} << EOF
@@ -225,14 +203,10 @@ function update_route53
     ]
 }
 EOF
-
-#backup the current record sets first
 backfile="/etc/httpd/route53/backup-"$dt".json"
 echo "creating backup of existing Route53 records : "$backfile
 aws route53 list-resource-record-sets --hosted-zone-id $hostedzoneid > $backfile
-#use the temporary file to create a record on Route53 using the AWS cli
 aws route53 change-resource-record-sets --hosted-zone-id $hostedzoneid --change-batch file://$TMPFILE
-#Remove the temporary file
 rm -f $TMPFILE
 }
 
